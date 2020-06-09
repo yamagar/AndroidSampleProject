b3a89c3db80d0bc9421268d73855961fb64d49d9
yamagarsan-hub <yama.gar.san@gmail.com>
2020-06-09 09:52:58 +0900

    ボタン押下時のイベントハンドラを追加 書籍だとJavaで記述されており、実際のソースはKotlinなので、その差異に四苦八苦。 WEBで検索して対応した。参考URLは以下の通り https://www.programming-shimomura.com/2018/02/24/%E3%80%90android-kotlin%E3%80%91onclicklistener%E8%A8%AD%E5%AE%9A%E4%B8%80%E8%A6%A7/

---
+++ HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.websarva.wings.android.hellosample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hello_sample)
+    }
+
+    ///
+    /// 参考URL:https://www.programming-shimomura.com/2018/02/24/%E3%80%90android-kotlin%E3%80%91onclicklistener%E8%A8%AD%E5%AE%9A%E4%B8%80%E8%A6%A7/
+    ///
+    fun buttonClickEvent(view: View) {
+        Log.d("MainActivity", "button click")
+    }
+}

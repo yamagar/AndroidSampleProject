8778833795d12f4b82502bca085364ba3d33569a
yamagarsan-hub <yama.gar.san@gmail.com>
2020-11-23 11:52:57 +0900

    2020/11/23 第4章ようやく終了。findViewByID()とview.getId()を使う。次は第5章からスタート。

--- HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
@@ -13,10 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hello_sample)
     }
 
+/*
     ///
     /// 参考URL:https://www.programming-shimomura.com/2018/02/24/%E3%80%90android-kotlin%E3%80%91onclicklistener%E8%A8%AD%E5%AE%9A%E4%B8%80%E8%A6%A7/
     ///
     fun buttonClickEvent(view: View) {
         Log.d("MainActivity", "button click")
     }
+*/
+    private class HelloListenerimplements View.OnClickListener{
+        @Override
+        public void onClick(view view) {
+        }
+    }
+
 }
